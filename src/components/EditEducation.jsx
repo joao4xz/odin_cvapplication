213855e7e1ff111f0751293926500de15331ffc2
joao4xz <joao.4xz@gmail.com>
2023-10-09 09:22:33 -0300

    Add education edit section

--- src/components/EditEducation.jsx
+++ src/components/EditEducation.jsx
@@ -1,3 +1,51 @@
 export default function EditEducation() {
-  return <></>;
+  return (
+    <div className="bg-slate-100 p-3 rounded-xl">
+      <h2 className="font-bold text-xl">Education</h2>
+      <form className="flex flex-col gap-3 p-3">
+        <label className="flex flex-col" htmlFor="university">
+          University:
+          <input
+            className="rounded-lg bg-slate-200"
+            type="text"
+            name="university"
+          />
+        </label>
+        <label className="flex flex-col" htmlFor="educationLocation">
+          Location:
+          <input
+            className="rounded-lg bg-slate-200"
+            type="text"
+            name="educationLocation"
+          />
+        </label>
+        <label className="flex flex-col" htmlFor="degree">
+          Degree:
+          <input
+            className="rounded-lg bg-slate-200"
+            type="text"
+            name="degree"
+          />
+        </label>
+        <div>
+          <label className="mr-3" htmlFor="startDate">
+            Start Date:
+            <input
+              className="rounded-lg bg-slate-200 w-[130px] ml-2 mt-2"
+              type="text"
+              name="startDate"
+            />
+          </label>
+          <label className="" htmlFor="endDate">
+            End Date:
+            <input
+              className="rounded-lg bg-slate-200 w-[130px] ml-2 mt-2"
+              type="text"
+              name="endDate"
+            />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
